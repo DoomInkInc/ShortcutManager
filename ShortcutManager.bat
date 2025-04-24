9fc84c50c19d956ded8ae4b2f6c892997497ef5b
ItsDoomOne <85998702+ItsDoomOne@users.noreply.github.com>
2025-04-23 21:19:19 -0300

    Fixed exitSequence (es) and added exitDelay correctly

--- ShortcutManager.bat
+++ ShortcutManager.bat
@@ -1,63 +1,50 @@
 ::THE FRICKING BOOTSTRAPPER
 :bootStrapper
-
-  @echo off 
-  chcp 65001 > nul
-  setlocal EnableDelayedExpansion
-  set PATH=%~dp0bin;%PATH%
+@echo off 
+chcp 65001 > nul
+setlocal EnableDelayedExpansion
+set PATH=%~dp0bin;%PATH%
 
 
 ::CHOOSE YOUR FATE Part I
 :fate1
-  cls
-  echo   ____    _____   _____   __  __   _   ____  
-  echo  ^|  _ \  ^|  _  ^| ^|  _  ^| ^|  \/  ^| ^| ^| / ___^| 
-  echo  ^| ^| ^| ^| ^| ^| ^| ^| ^| ^| ^| ^| ^| ^|\/^| ^| ^|_^| \___ \ 
-  echo  ^| ^|_^| ^| ^| ^|_^| ^| ^| ^|_^| ^| ^| ^|  ^| ^|     ___) ^|
-  echo  ^|____/  ^|_____^| ^|_____^| ^|_^|  ^|_^|    ^|____/ 
-  echo.
-  echo               ShortCut Control
-  echo.
-  echo Created by Doom, mantained by DoomInk
-  echo.
-  echo Choose an option:
-  echo 1- Add a shortcut
-  echo 2- Open the shortcut list
-  echo 3- Remove a Shortcut
-  echo 4- Exit
+cls
+echo   ____    _____   _____   __  __   _   ____  
+echo  ^|  _ \  ^|  _  ^| ^|  _  ^| ^|  \/  ^| ^| ^| / ___^| 
+echo  ^| ^| ^| ^| ^| ^| ^| ^| ^| ^| ^| ^| ^| ^|\/^| ^| ^|_^| \___ \ 
+echo  ^| ^|_^| ^| ^| ^|_^| ^| ^| ^|_^| ^| ^| ^|  ^| ^|     ___) ^|
+echo  ^|____/  ^|_____^| ^|_____^| ^|_^|  ^|_^|    ^|____/ 
+echo.
+echo               ShortCut Control
+echo.
+echo Created by Doom, mantained by DoomInk
+echo.
+echo Choose an option:
+echo 1- Add a shortcut
+echo 2- Open the shortcut list
+echo 3- Remove a Shortcut
+echo 4- Exit
 
 ::CHOOSE YOUR FATE Part II
 :fate2
+set /p CHOOSEYOURFATE="Select: "
+if "%CHOOSEYOURFATE%"=="1" goto AddShortcut
+if "%CHOOSEYOURFATE%"=="2" goto OpenShortcutList
+if "%CHOOSEYOURFATE%"=="3" goto RemoveShortcut
+if "%CHOOSEYOURFATE%"=="4" goto exitSequence
+cls
+echo It appears you typed an option that does not exist.
+echo Press enter to try again.
+pause > nul
+goto fate1
 
-  set /p CHOOSEYOURFATE="Select: "
-  if "%CHOOSEYOURFATE%"=="1" goto AddShortcut
-  if "%CHOOSEYOURFATE%"=="2" goto OpenShortcutList
-  if "%CHOOSEYOURFATE%"=="3" goto RemoveShortcut
-  if "%CHOOSEYOURFATE%"=="4" call exitDelay 500
+::FUNCTIONS
 
-  cls
-  echo It appears you typed an option that does not exist.
-  echo Press enter to try again.
-  pause > nul
-  goto fate1
-
-
-
-
-
-
-
-
-   
-
-
-
-  ::FUNCTIONS
-
-
-  :pause1point5
-  powershell -c "Start-Sleep -Milliseconds 300"
-  goto :eof
+:pause1point5
+powershell -c "Start-Sleep -Milliseconds 300"
+goto :eof
   
-
+:exitSequence
+call exitDelay 500
+exit /b
   
