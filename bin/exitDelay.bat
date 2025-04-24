b7f9c3d7aa64478be1113bec8832d2824f2502ed
ItsDoomOne <85998702+ItsDoomOne@users.noreply.github.com>
2025-04-23 21:16:41 -0300

    exitDelay created

---
+++ bin/exitDelay.bat
@@ -0,0 +1,13 @@
+  cls
+  echo It appears you have chosen to exit.
+  echo Exiting in 3...
+  powershell -c "Start-Sleep -Milliseconds %1"
+  cls
+  echo It appears you have chosen to exit.
+  echo Exiting in 2...
+  powershell -c "Start-Sleep -Milliseconds %1"
+  cls
+  echo It appears you have chosen to exit.
+  echo Exiting in 1...
+  powershell -c "Start-Sleep -Milliseconds %1"
+  exit /b
